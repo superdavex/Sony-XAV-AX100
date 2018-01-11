de50fe176664ec2252169970c3efc488ed63cb54
David Bass <david.bass@prosoft.us.com>
2018-01-11 16:35:35 -0500

    Added automated windows batch file version.

--- ChangeLogo/README.md
+++ ChangeLogo/README.md
@@ -2,6 +2,12 @@
 
 Logo requires a 800x480 image.  Any format should work.  If the image is not 800x480 it will resized.
 
+# Added windows batch file version.  
+1. Extract all the files in logo.zip to your pc.
+2. Run createXAVlogo.bat and follow the prompts.
+
+
+# Original cygwin version
 This release requires cygwin and my modified firmware to be installed. 
 
 1. Install cygwin
---
+++ ChangeLogo/createXAVlogo.bat
@@ -0,0 +1,80 @@
+@echo off 
+setlocal EnableDelayedExpansion
+set "input="
+set "outsize="
+
+set /p input=Enter the 800x480 image input file name: 
+
+REM 800 x 480, 32 bit depth, uncompressed. 72dpih 72dpiv
+SET outname="newlogo.img"
+SET logo_len=1537024
+
+if not exist %input% (
+   echo A valid input image is required
+   goto failed
+)
+
+IF exist %outname% (
+    del %outname%
+)
+
+
+REM #shrink/stretch
+REM #./convert -compress none -resize 800x480! -verbose -interlace none -depth 8 $input tmpoutput.rgba
+REM #scale and add borders
+convert -compress none -resize 800x480 -verbose -interlace none -background black -gravity center -depth 8 -extent 800x480 %input% tmpoutput.rgba
+
+IF exist tmpoutput.rgba (
+
+    copy logo.pre %outname%
+    type tmpoutput.rgba >> %outname%
+    type logo.pad >> %outname%
+
+    convert -compress none -size 800x480! -depth 8 tmpoutput.rgba converted_preview.tiff
+    REM #rm tmpoutput.rgba
+    echo.
+    echo.
+
+    FOR %%A IN (%outname%) DO (
+        if /I NOT %%~zA EQU %logo_len% (
+            echo Output file size %%~zA does not match correct size %logo_len%, do not use
+            goto failed
+        )
+    )
+    echo Image conversion complete.  
+    
+    set /p usbdrive=Enter the drive letter to copy files to:
+    set usbdrive=!usbdrive:~0,1!
+    echo !usbdrive!
+    
+    copy usbexecute !usbdrive!:\
+    if not exist "!usbdrive!:\usbexecute" (
+        goto failed_to_copy 
+    )
+    
+    copy flash_erase "!usbdrive!:\"
+    if not exist "!usbdrive!:\flash_erase" (
+        goto failed_to_copy 
+    )
+    copy newlogo.img "!usbdrive!:\"
+    copy newlogo.img "!usbdrive!:\"
+    if not exist "!usbdrive!:\newlogo.img" (
+        goto failed_to_copy 
+    )
+    
+    echo usbexecute, flash_erase, and newlogo.img have been copied.  Insert usb stick into radio.  Radio will reboot when complete.  Remove the drive as soon as it reboots.
+    ECHO Press any key to exit
+    PAUSE >NUL
+    EXIT /B
+    
+)    
+
+exit /b
+
+:failed_to_copy
+    echo failed to copy file to !usbdrive!
+:failed
+   echo Image conversion failed
+
+
+
